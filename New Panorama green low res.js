56eb694f6769d7f7918a29716f7572cb713dd074
georgefakhry <georgefakhry.creative@gmail.com>
2026-09-23 15:58:34 +0300

    Add files via upload

---
+++ New Panorama green low res.js
@@ -0,0 +1,51 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "New Panorama green low res_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 63.523504351555545,
+	        "minpan": 0,
+	        "mintilt": -90
+	    },
+	"id": "New Panorama green low res",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_1b_%y_%x.jpg"},"down":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_1d_%y_%x.jpg"},"front":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_1f_%y_%x.jpg"},"height":791,"left":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_1l_%y_%x.jpg"},"right":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_1r_%y_%x.jpg"},"tilesize":480,"up":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_1u_%y_%x.jpg"},"width":791},{"back":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_2b_%y_%x.jpg"},"down":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_2d_%y_%x.jpg"},"front":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_2f_%y_%x.jpg"},"height":396,"left":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_2l_%y_%x.jpg"},"right":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_2r_%y_%x.jpg"},"tilesize":480,"up":{"src":"New Panorama green low res_tiles/New Panorama green low res_t_2u_%y_%x.jpg"},"width":396}],"multilevel":true,"preview":{"src":"New Panorama green low res_tiles/New Panorama green low res_preview.jpg"},"projection":"cubic"},
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #000000, 0px 0px 2.5px #000000, 0px 0px 1.7px #000000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "Zeiina Soliman Art Work / George Fakhry Photography-Panorama 720",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
